ffd161e7b36669c635e3d3e278e3f519949f38db
Marcel Becheanu <marcel.becheanu@outlook.pt>
2023-03-07 03:38:59 +0000

    feat(seo): Adding meta tags for improve the seo.

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,16 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="shortlink" href="https://lightcode.pt/" />
+        <link rel="canonical" href="https://www.lightcode.pt/" />
+
+        <link rel="alternate" hreflang="en" href="https://lightcode.pt/pt/" />
+        <link rel="alternate" hreflang="pt" href="https://lightcode.pt/en/" />
+        <link rel="alternate" hreflang="x-default" href="https://lightcode.pt/" />
+      
+        <meta name="robots" content="all" />
+
         <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
       </Head>
       <body>
